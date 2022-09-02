56570fc291e4dafa4aae639ac6a8ee40d3ad1526
mulcai <iain.mulcahy@dell.com>
2022-09-02 16:12:48 +0100

    Modified

--- scripts/Understat/get_league_players.py
+++ scripts/Understat/get_league_players.py
@@ -6,21 +6,26 @@ import aiohttp
 
 from understat import Understat
 
-team = ""
 year = ""
-team = sys.argv[1]
-year = sys.argv[2]
+league = ""
+league = sys.argv[1]
+games = sys.argv[2]
+year = sys.argv[3]
 
-async def main(team: str, year: str):
+async def main(league: str, year: str, games: str):
+    
     async with aiohttp.ClientSession() as session:
-        understat = Understat(session)
-        if team == "All":             
-            data = await understat.get_league_players("epl", year)
-        else:
-            data = await understat.get_league_players("epl", year, {"team_title": team})
+        options = {
+            "n_last_matches": 1
+        }
+        if (games != "0"):
+            options["n_last_matches"] = int(games)
 
+        understat = Understat(session)
+        data = await understat.get_league_players(league, year,position="F S",date_start="2022-08-08")
+        
         print(json.dumps(data))
 
 
 loop = asyncio.get_event_loop()
-loop.run_until_complete(main(team, year))
+loop.run_until_complete(main(league, year, games))
--- src/Context.ts
+++ src/Context.ts
@@ -1,29 +1,58 @@
 import * as Express from 'express'
 import ConfigHelper from './ConfigHelper'
+import fetch from "node-fetch"
+import { QueryParameters } from 'util-commons/QueryParametersEnconding'
+import * as EJSON from 'util-commons/EjsonParser';
 
 const app = Express()
 
 import {Options, PythonShell} from 'python-shell';
 
+interface IPlayers{
+    league: string
+    season: number
+}
  
 export async function initApplicationContext(configWrapper: any = new ConfigHelper()){
 
     let path = '.' + configWrapper.getConfigValue("scriptPath", "/scripts/Understat/")
 
-    app.get('/api/players', function(req, res) {
-        let team = req.query.team ? req.query.team : "All"
-        let year = req.query.year ? req.query.year : 2020
-        let options: Options = {
-            mode: 'json',
-            scriptPath: path,
-            args: [team.toString(), year.toString()]
-        };
+    const getCurrentYear = () => {
+        return new Date().getFullYear()
+    }
 
-        console.log('Running for %j %s', team, year);
-        
-        PythonShell.run('get_league_players.py', options, function (err, results) {
-            handleResult(err, res, results)
-        });
+    app.get('/api/players', async function(req, res) {
+        let league = req.query.league ? req.query.league.toString() : "EPL"
+        let season = req.query.season ? parseInt(req.query.season.toString()) : getCurrentYear()       
+        let games = req.query.games ? parseInt(req.query.games.toString()) : undefined
+        let position: string[] = req.query.position ? QueryParameters.decodeQueryParameterValue(req.query.position as string[]) : undefined         
+
+        const bodyText = () => {
+            let body = `league=${league}&season=${season}`
+            body = games ? `${body}&n_last_matches=${games}` : body
+            if (position.length > 2){
+                for (let i=1;i<position.length-1;i++){                     
+                    body = `${body}&positions%5B%5D=${position[i]}`               
+                }
+            }
+            return body
+        }
+        let response = await fetch('https://understat.com/main/getPlayersStats/',{
+            headers: {
+                'Accept':'application/json',
+                'Content-Type':'application/x-www-form-urlencoded'
+            },
+            method: "POST",
+            body: bodyText() 
+        })
+        const data = await response.json();
+
+        if (data.error){
+            res.status(400).send({error: data.error.error_msg, status: "Fail"})  
+        }
+        else{
+            res.status(200).send({results: data.response.players, status: "OK"})  
+        }
 
     }); 
 
@@ -39,7 +68,7 @@ export async function initApplicationContext(configWrapper: any = new ConfigHelp
             scriptPath: path,
             args: [playerID.toString()]
         };
-
+        
         console.log('Running for player %j', playerID);
         
         PythonShell.run('get_league_player_stats.py', options, function (err, results) {
@@ -50,7 +79,7 @@ export async function initApplicationContext(configWrapper: any = new ConfigHelp
 
     app.get('/api/team_stats', function(req, res) {
         let team = req.query.team ? req.query.team : ""
-        let year = req.query.year ? req.query.year : 2020 //TO DO get current year
+        let year = req.query.year ? req.query.year : getCurrentYear() //TO DO get current year
         if (team === ""){  
              "Team not provided in API call"
         }
@@ -70,7 +99,7 @@ export async function initApplicationContext(configWrapper: any = new ConfigHelp
 
     app.get('/api/teams', function(req, res) {
         let league = req.query.league ? req.query.league : ""
-        let year = req.query.year ? req.query.year : 2020 //TO DO get current year
+        let year = req.query.year ? req.query.year : getCurrentYear()  // returns the current year //TO DO get current year
         if (league === ""){  
              "League not provided in API call"
         }
